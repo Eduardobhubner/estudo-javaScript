284aede75f65ac34dcc16c01525cc0b1f789c24e
Eduardo balan Hubner <eduardobhubner@gmail.com>
2021-06-18 17:53:51 -0300

    Alteração de arquivos

--- projeto-O.O/Funcionario/Funcionario.js
+++ projeto-O.O/Funcionario/Funcionario.js
@@ -1,11 +1,20 @@
 export class Funcionario {
 
+    get senha(){
+        return this._senha;
+    }
+
     constructor(nome, salario, cpf){
         this._nome = nome;
         this._salario = salario;
         this._cpf = cpf;
         this._bonificacao = 1;
+        this._senha;
     }
 
-    
+    cadastrarSenha(senha){
+        this._senha = senha;
+    }
+
+
 } 
--- projeto-O.O/index.js
+++ projeto-O.O/index.js
@@ -1,27 +1,19 @@
 //2021-06-09 Ainda não existe no JS uma maneira formal de determinar um atributo privado a classe, já se fala sobre usar '#' antes(ex: #nome), porem ainda não foi solitificado 100% no mercado
 //hoje para falar que um atributo da classe é privada usados '_' antes(ex: _nome), porem dessa forma é apenas uma boa prática, ainda podendo alterar o valor mesmo fora da classe 
 
-import { Cliente } from './Cliente.js';
-import { ContaCorrente } from './Conta/ContaCorrente.js';
-import { ContaPoupanca } from './Conta/ContaPoupanca.js';
-import { ContaSalario } from './Conta/ContaSalario.js';
+// import { Cliente } from './Cliente.js';
+import { Diretor } from './Funcionario/Diretor.js'
+import { Gerente } from './Funcionario/Gerente.js'
+import { SistemaAutenticacao } from './Funcionario/SistemaAutenticacao.js'
 
+const diretor = new Diretor('Mauricio', 10000, 13245678901);
+const gerente = new Gerente('sandro', 5000, 78945612397);
 
-const cliente1 = new Cliente('Eduardo', 1122233309);
+diretor.cadastrarSenha("123456789");
+gerente.cadastrarSenha("123123");
 
-const cliente1Corrente = new ContaCorrente(200, cliente1 , 1001);
-const Cliente1Poupanca = new ContaPoupanca(150, cliente1, 1001);
-const cliente1Salario = new ContaSalario(cliente1);
-
-cliente1Corrente.sacar(50);
-Cliente1Poupanca.sacar(25);
-
-// const cliente1Poupanca = new ContaPoupanca(0, cliente1, 1001);
-
-console.log(cliente1Corrente);
-console.log(Cliente1Poupanca);
-console.log(cliente1Salario);
-
+const estaLogado = SistemaAutenticacao.login(gerente, "123123");
+console.log(estaLogado);
 
 
 
